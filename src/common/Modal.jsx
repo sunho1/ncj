64cd627e8e10ebbb4bd2ebc3fa07a3ffafe195f6
sunho1 <87847307+sunho1@users.noreply.github.com>
2023-01-09 23:04:35 +0900

    first commit

---
+++ src/common/Modal.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ModalPage(){
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default ModalPage;
---
+++ src/pages/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Main.module.css"
+
+function MainPage(){
+    return(
+        <div className={styles.main}>
+            hihihihi
+        </div>
+    )
+    
+}
+
+export default MainPage;
